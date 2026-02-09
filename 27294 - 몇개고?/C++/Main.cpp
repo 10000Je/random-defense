cafceca2c861b9ad1c594f97253f92aa1534e64c
10000Je <ssh030226@gmail.com>
2026-02-09 18:41:33 +0900

    27294. 몇개고?

---
+++ 27294 - 몇개고?/C++/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T, S;
+    cin >> T >> S;
+
+    if (S == 0 && T >= 12 && T <= 16)
+        cout << 320 << '\n';
+    else
+        cout << 280 << '\n';
+
+    return 0;
+}
